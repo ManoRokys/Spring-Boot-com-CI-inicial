6e20337c2d5439f052cbe53a61c60cb57d13629b
DSM4 <fatec-dsm4@academico.intra>
2025-10-29 20:06:04 -0300

    Atividade 1

---
+++ projeto-ci-inicial-lucasgf/src/main/java/br/com/atv/projeto_ci_inicial_lucasgf/ProjetoCiInicialLucasgfApplication.java
@@ -0,0 +1,13 @@
+package br.com.atv.projeto_ci_inicial_lucasgf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCiInicialLucasgfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCiInicialLucasgfApplication.class, args);
+	}
+
+}
---
+++ projeto-ci-inicial-lucasgf/src/test/java/br/com/atv/projeto_ci_inicial_lucasgf/ProjetoCiInicialLucasgfApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.atv.projeto_ci_inicial_lucasgf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCiInicialLucasgfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
